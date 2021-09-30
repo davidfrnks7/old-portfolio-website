e790e0c352f5616188709a564139eb063ef7a0f6
David F <davidfrnks7@gmail.com>
2021-09-30 11:27:12 -0400

    Fix greet background height

--- components/sections/greeting/Greeting.tsx
+++ components/sections/greeting/Greeting.tsx
@@ -8,7 +8,7 @@ const Greeting = (): JSX.Element => {
     <Box
       w="100%"
       h="100vh"
-      mt="-48px"
+      mt="-60px"
       px={{ base: "3rem", lg: "3rem", xl: "5rem" }}
       textAlign="center"
       className="background"
