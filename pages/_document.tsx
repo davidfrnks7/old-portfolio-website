aae2076e72a6a802851470b59839ec2d234d93f8
David F <davidfrnks7@gmail.com>
2021-10-01 14:56:17 -0400

    More og tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,26 +8,26 @@ class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/images/favicon.svg" />
-          <meta property="og:site_name" content="David's Portfolio Website" />
+          <link rel="icon" href="/images/favicon.svg" sizes="32x32 192x192" />
+          <link rel="apple-touch-icon" href="/images/favicon.svg" />
+          <meta property="og:title" content="David's Portfolio Website" />
+          <meta
+            name="og:description"
+            content="A website to list my skills and demonstrate them for potential employers and clients to see."
+          />
           <meta property="og:image" content="/images/davidFImage.jpg" />
           <meta property="og:image:type" content="image/jpeg" />
           <meta property="og:image:alt" content="A face shot of David Franks." />
+          <meta property="og:url" content="https://davidfrnks7.dev" />
+          <meta name="twitter:card" content="summary_large_image" />
           <meta httpEquiv="content-language" content="en_US" />
           <meta charSet="UTF-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
+          <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0" />
           <meta
             name="keywords"
             content="portfolio software engineering coding developer seeking employment open for hire"
           />
           <meta name="copyright" content="David Franks" />
-          <meta
-            name="description"
-            content="A website to list my skills and demonstrate them for potential employers and clients to see."
-          />
           <meta name="page-topic" content="Skills and PRojects" />
           <meta name="page-type" content="Portfolio" />
           <meta name="audience" content="Everyone" />
