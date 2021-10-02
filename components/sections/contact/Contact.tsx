f91fa9eaa7acd3e6a70a16b40b7a1a60d26714b3
David F <davidfrnks7@gmail.com>
2021-10-01 23:17:20 -0400

    form border on mobile

--- components/sections/contact/Contact.tsx
+++ components/sections/contact/Contact.tsx
@@ -158,7 +158,7 @@ const Contact = (): JSX.Element => {
       justifyContent="center"
       alignContent="center"
       py="10%"
-      spacing={6}
+    spacing={6}
       id="contact"
     >
       <Link name="contact" />
@@ -167,9 +167,9 @@ const Contact = (): JSX.Element => {
           base: "none",
           md: "rgba(255, 255, 255, 0.2) 0px 0px 15px, rgba(255, 255, 255, 0.15) 0px 0px 3px 1px",
         }}
-        borderRadius={{ base: "none", md: "2xl" }}
-        border={{ base: "none", md: "1px solid white" }}
-        w="90%"
+        borderRadius="2xl"
+        border="1px solid white"
+        w={{ base: "95%", lg: "90%" }}
         zIndex={1}
         h="100%"
         py={12}
